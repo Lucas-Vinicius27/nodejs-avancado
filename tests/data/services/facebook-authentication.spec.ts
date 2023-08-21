45b95eeda10154fa044468032609f9a2f8a23638
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-08-21 09:34:46 -0300

    test: change mock LoadFacebookUserApi

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,9 +1,11 @@
+import { mock } from 'jest-mock-extended'
 import { FacebookAuthenticationService } from '@/domain/data/services'
 import { AuthenticationError } from '@/domain/errors'
+import { type LoadFacebookUserApi } from '@/domain/data/contracts/apis'
 
 describe('FacebookAuthenticationService', () => {
   it('should call LoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApi = { loadUser: jest.fn() }
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
     await sut.perform({ token: 'any_token' })
@@ -13,7 +15,7 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    const loadFacebookUserApi = { loadUser: jest.fn() }
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
 
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
