412a12e234118d76b13e58673179634164b144a0
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-08-23 12:58:37 -0300

    feat: apply create facebook account repository

--- src/domain/data/services/facebook-authentication.ts
+++ src/domain/data/services/facebook-authentication.ts
@@ -1,12 +1,16 @@
 import { type FacebookAuthentication } from '@/domain/feature'
 import { type LoadFacebookUserApi } from '@/domain/data/contracts/apis'
-import { type LoadUserAccountRepository } from '@/domain/data/contracts/repos'
+import {
+  type LoadUserAccountRepository,
+  type CreateFacebookAccountRepository
+} from '@/domain/data/contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly loadFacebookUserApi: LoadFacebookUserApi,
-    private readonly loadUserAccountRepo: LoadUserAccountRepository
+    private readonly loadUserAccountRepo: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepo: CreateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
@@ -14,6 +18,7 @@ export class FacebookAuthenticationService {
 
     if (fbData !== undefined) {
       await this.loadUserAccountRepo.load({ email: fbData.email })
+      await this.createFacebookAccountRepo.createFromFacebook(fbData)
     }
 
     return new AuthenticationError()
