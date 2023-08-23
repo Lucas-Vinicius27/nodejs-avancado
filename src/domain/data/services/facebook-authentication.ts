e1cf7ece032273eb6139751a77d7f91cb5fc05a7
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-08-23 12:04:01 -0300

    feat: apply load account user repository

--- src/domain/data/services/facebook-authentication.ts
+++ src/domain/data/services/facebook-authentication.ts
@@ -1,14 +1,21 @@
 import { type FacebookAuthentication } from '@/domain/feature'
 import { type LoadFacebookUserApi } from '@/domain/data/contracts/apis'
+import { type LoadUserAccountRepository } from '@/domain/data/contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 
 export class FacebookAuthenticationService {
   constructor (
-    private readonly loadFacebookUserApi: LoadFacebookUserApi
+    private readonly loadFacebookUserApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepo: LoadUserAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.loadFacebookUserApi.loadUser(params)
+    const fbData = await this.loadFacebookUserApi.loadUser(params)
+
+    if (fbData !== undefined) {
+      await this.loadUserAccountRepo.load({ email: fbData.email })
+    }
+
     return new AuthenticationError()
   }
 }
