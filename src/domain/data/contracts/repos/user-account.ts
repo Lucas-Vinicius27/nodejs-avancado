096014108bf71767d2afaa70c6d90e0bcfa19b5e
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-08-23 22:24:00 -0300

    feat: returns a specific object

--- src/domain/data/contracts/repos/user-account.ts
+++ src/domain/data/contracts/repos/user-account.ts
@@ -7,5 +7,8 @@ export namespace LoadUserAccountRepository{
     email: string
   }
 
-  export type Result = undefined
+  export type Result = undefined | {
+    id: string
+    name?: string
+  }
 }
