c4ecfb1440f81e99ab447933b3362e60ba11074a
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-08-28 14:09:51 -0300

    feat: change type params

--- src/infra/http/client.ts
+++ src/infra/http/client.ts
@@ -5,10 +5,6 @@ export interface HttpGetClient {
 export namespace HttpGetClient {
   export type Params = {
     url: string
-    params: {
-      client_id: string
-      client_secret: string
-      grant_type: string
-    }
+    params: object
   }
 }
