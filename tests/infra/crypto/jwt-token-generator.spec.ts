d2d7511fd31a5270788ea24f89bec7894ad5aa2e
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-08-29 16:12:50 -0300

    test: isolate SUT creation

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -13,15 +13,25 @@ export class JwtTokenGenerator {
 }
 
 describe('JwtTokenGenerator', () => {
-  it('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
+  let secret: string
 
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+    secret = 'any_secret'
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator(secret)
+  })
+
+  it('should call sign with correct params', async () => {
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
 
     expect(fakeJwt.sign).toHaveBeenCalledWith(
       { key: 'any_key' },
-      'any_secret',
+      secret,
       { expiresIn: 1 }
     )
     expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
