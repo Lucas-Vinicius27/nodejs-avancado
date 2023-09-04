4633938a14a75631ac514dc6a0327443f501d04c
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-09-04 13:28:28 -0300

    test: ensure FacebookLgoinController returns 400 if token is undefined

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -34,4 +34,15 @@ describe('FacebookLgoinController', () => {
       data: new Error('The filed token is required')
     })
   })
+
+  it('should return 400 if token is undefined', async () => {
+    const sut = new FacebookLgoinController()
+
+    const httpResponse = await sut.handle({ token: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The filed token is required')
+    })
+  })
 })
