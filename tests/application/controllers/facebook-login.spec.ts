5b9a02949a0de27d63914018a3639ca75f2d22c3
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-09-04 13:27:22 -0300

    test: ensure FacebookLgoinController returns 400 if token is null

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -23,4 +23,15 @@ describe('FacebookLgoinController', () => {
       data: new Error('The filed token is required')
     })
   })
+
+  it('should return 400 if token is null', async () => {
+    const sut = new FacebookLgoinController()
+
+    const httpResponse = await sut.handle({ token: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The filed token is required')
+    })
+  })
 })
