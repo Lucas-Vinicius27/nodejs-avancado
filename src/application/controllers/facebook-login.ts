239b638092f44f2a7dd82add088dd80711b0c073
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-09-04 15:32:05 -0300

    feat: create FacebookLgoinController its own file

---
+++ src/application/controllers/facebook-login.ts
@@ -0,0 +1,42 @@
+import { type FacebookAuthentication } from '@/domain/feature'
+import { AccessToken } from '@/domain/models'
+import { type HttpResponse } from '@/application/helpers'
+import { ServerError } from '@/application/errors'
+
+export class FacebookLgoinController {
+  constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
+
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    try {
+      if (
+        httpRequest.token === '' ||
+        httpRequest.token === null ||
+        httpRequest.token === undefined
+      ) {
+        return {
+          statusCode: 400,
+          data: new Error('The filed token is required')
+        }
+      }
+
+      const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
+
+      if (result instanceof AccessToken) {
+        return {
+          statusCode: 200,
+          data: { accessToken: result.value }
+        }
+      }
+
+      return {
+        statusCode: 401,
+        data: result
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        data: new ServerError(error)
+      }
+    }
+  }
+}
