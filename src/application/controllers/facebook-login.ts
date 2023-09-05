ac39639c00c993162bf99704c92509c816a8fb63
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-09-05 11:27:27 -0300

    feat: create and apply type Model

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -7,10 +7,14 @@ type HttpRequest = {
   token: string | undefined | null
 }
 
+type Model = Error | {
+  accessToken: string
+}
+
 export class FacebookLgoinController {
   constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       if (
         httpRequest.token === '' ||
