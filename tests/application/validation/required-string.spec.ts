0a6a40670df28d56f0c32cdfe6106b62e76793b8
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-09-05 12:19:47 -0300

    test: ensure RequiredStringValidator returns Error if value is undefined

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -9,7 +9,8 @@ class RequiredStringValidator {
   validate (): Error | undefined {
     if (
       this.value === '' ||
-      this.value === null
+      this.value === null ||
+      this.value === undefined
     ) return new RequiredFieldError(this.fieldName)
   }
 }
@@ -25,6 +26,14 @@ describe('RequiredStringValidator', () => {
 
   it('should return RequiredFieldError if value is null', () => {
     const sut = new RequiredStringValidator(null as any, 'any_field_name')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field_name'))
+  })
+
+  it('should return RequiredFieldError if value is undefined', () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field_name')
 
     const error = sut.validate()
 
