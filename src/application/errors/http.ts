d3cf3361a7714f309c7aa3c5a9818b8c1528930e
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-09-05 10:44:18 -0300

    feat: create class RequiredFieldError

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -5,3 +5,10 @@ export class ServerError extends Error {
     this.stack = error instanceof Error ? error.stack : ''
   }
 }
+
+export class RequiredFieldError extends Error {
+  constructor (fieldName: string) {
+    super(`The filed ${fieldName} is required`)
+    this.name = 'RequiredFieldError'
+  }
+}
