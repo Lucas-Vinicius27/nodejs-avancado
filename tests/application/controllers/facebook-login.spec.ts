ab0d256763d7d6437a34da5c62c13665183dad17
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-09-11 09:06:21 -0300

    test: isolate creation variable

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -8,8 +8,10 @@ import { ServerError, UnauthorizedError } from '@/application/errors'
 describe('FacebookLgoinController', () => {
   let sut: FacebookLgoinController
   let facebookAuth: MockProxy<FacebookAuthentication>
+  let token: string
 
   beforeAll(() => {
+    token = 'any_token'
     facebookAuth = mock()
     facebookAuth.perform.mockResolvedValue(new AccessToken('any_value'))
   })
@@ -46,15 +48,15 @@ describe('FacebookLgoinController', () => {
   })
 
   it('should call FacebookAuthentication with correct params', async () => {
-    await sut.handle({ token: 'any_token' })
+    await sut.handle({ token })
 
-    expect(facebookAuth.perform).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(facebookAuth.perform).toHaveBeenCalledWith({ token })
     expect(facebookAuth.perform).toHaveBeenCalledTimes(1)
   })
 
   it('should return 401 if authentication fails', async () => {
     facebookAuth.perform.mockResolvedValueOnce(new AuthenticationError())
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
 
     expect(httpResponse).toEqual({
       statusCode: 401,
@@ -63,7 +65,7 @@ describe('FacebookLgoinController', () => {
   })
 
   it('should return 200 if authentication succeeds', async () => {
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
 
     expect(httpResponse).toEqual({
       statusCode: 200,
@@ -74,7 +76,7 @@ describe('FacebookLgoinController', () => {
   it('should return 500 if authentication throws', async () => {
     const error = new Error('infra_error')
     facebookAuth.perform.mockRejectedValueOnce(error)
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
 
     expect(httpResponse).toEqual({
       statusCode: 500,
