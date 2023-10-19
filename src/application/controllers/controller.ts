3facda3a7153626316068df9fe69c00f44fca1ee
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-10-19 12:21:33 -0300

    feat: creating Controller abstract class

---
+++ src/application/controllers/controller.ts
@@ -0,0 +1,26 @@
+import { badRequest, type HttpResponse, serverError } from '@/application/helpers'
+import { ValidationComposite, type Validator } from '@/application/validation'
+
+export abstract class Controller {
+  abstract perform (httpRequest: any): Promise<HttpResponse>
+
+  buildValidators (httpRequest: any): Validator[] {
+    return []
+  }
+
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    try {
+      const error = this.validate(httpRequest)
+
+      if (error !== undefined) return badRequest(error)
+
+      return await this.perform(httpRequest)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+
+  private validate (httpRequest: any): Error | undefined {
+    return new ValidationComposite(this.buildValidators(httpRequest)).validate()
+  }
+}
