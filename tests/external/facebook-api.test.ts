64c35c418e6fb46e3731bc49d26be1773698812b
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-23 13:01:13 -0300

    test: creating testing integration with FacebookApi

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,4 +1,29 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
+describe('FacebookApi', () => {
+  let axiosClient: AxiosHttpClient
+  let sut: FacebookApi
+
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient()
+    sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+  })
+
+  it('should return a Facebook User if token is valid', async () => {
+    const fbUser = await sut.loadUser({ token: '' })
+
+    expect(fbUser).toEqual({
+      name: '',
+      email: '',
+      facebookId: ''
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
