8928d364d1f4bcad593e9fe7d89966c30eb8a991
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-27 23:36:13 -0300

    feat: applying http server and configs

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,7 @@
+import './config/modules-alias'
 import 'reflect-metadata'
+import 'dotenv/config'
+import { app } from '@/main/config/app'
+import { env } from '@/main/config/env'
+
+app.listen(env.port, () => { console.log(`Server is running at http://localhost:${env.port}`) })
