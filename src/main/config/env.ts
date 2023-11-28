139f5013ed567eca73d2568ea0d0240de9826b95
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-27 23:08:27 -0300

    feat: creating prop port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: String(process.env.FB_CLIENT_ID) ?? '123456',
     clientSecret: String(process.env.FB_CLIENT_SECRET) ?? '123456'
-  }
+  },
+  port: Number(process.env.PORT) ?? 8080
 }
