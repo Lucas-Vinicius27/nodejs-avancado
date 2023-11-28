00f6e695929bd299cd1d67a9e364057f7b817eb0
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-27 23:33:29 -0300

    feat: creating setupRoutes

---
+++ src/main/config/routes.ts
@@ -0,0 +1,14 @@
+import { type Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+export const setupRoutes = (app: Express): void => {
+  const router = Router()
+  router.get('/', (req, res) => { res.send({ data: 'API is running!' }) })
+  readdirSync(join(__dirname, '../routes'))
+    .filter(file => !file.endsWith('.map'))
+    .map(async file => {
+      (await import(`../routes/${file}`)).default(router)
+    })
+  app.use(router)
+}
