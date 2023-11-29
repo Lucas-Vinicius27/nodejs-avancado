bc59b1d4c6f469b7920643b5f0b75d7c91e4b115
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-28 23:10:44 -0300

    feat: adding prop jwtSecret

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
     clientId: String(process.env.FB_CLIENT_ID) ?? '123456',
     clientSecret: String(process.env.FB_CLIENT_SECRET) ?? '123456'
   },
-  port: Number(process.env.PORT) ?? 8080
+  port: Number(process.env.PORT) ?? 8080,
+  jwtSecret: String(process.env.JWT_SECRET) ?? 'dad656'
 }
