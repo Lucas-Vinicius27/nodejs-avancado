a551de1fa8c42d0cde53f29ad257afb9272ac011
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-28 23:46:13 -0300

    feat: creating makeFacebookAuthenticationService

---
+++ src/main/factories/services/facebook-authentication.ts
@@ -0,0 +1,12 @@
+import { FacebookAuthenticationService } from '@/data/services'
+import { makePgUserAccountRepo } from '@/main/factories/repositories'
+import { makeJwtTokenGenerator } from '@/main/factories/crypto'
+import { makeFacebookApi } from '@/main/factories/apis'
+
+export const makeFacebookAuthenticationService = (): FacebookAuthenticationService => {
+  return new FacebookAuthenticationService(
+    makeFacebookApi(),
+    makePgUserAccountRepo(),
+    makeJwtTokenGenerator()
+  )
+}
