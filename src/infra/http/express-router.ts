becf83ecd13d6c473d9a72565214cb2c60a78cc8
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-30 18:32:51 -0300

    refactor: improve code quality

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -4,11 +4,8 @@ import { type Controller } from '@/application/controllers'
 export const adaptExpressRoute = (controller: Controller): RequestHandler => {
   /* eslint @typescript-eslint/no-misused-promises: 0 */
   return async (req, res) => {
-    const result = await controller.handle({ ...req.body })
-    if (result.statusCode === 200) {
-      res.status(result.statusCode).json(result.data)
-    } else {
-      res.status(result.statusCode).json({ error: result.data.message })
-    }
+    const { data, statusCode } = await controller.handle({ ...req.body })
+    const json = statusCode === 200 ? data : { error: data.message }
+    res.status(statusCode).json(json)
   }
 }
