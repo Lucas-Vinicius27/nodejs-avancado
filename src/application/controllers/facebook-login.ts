6d8ab391fe6d8ec42ec77b610e25d85b83fb75f7
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-30 18:31:46 -0300

    feat: improve code quality

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -17,17 +17,17 @@ export class FacebookLoginController extends Controller {
     super()
   }
 
-  async perform (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
-    const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
+  async perform ({ token }: HttpRequest): Promise<HttpResponse<Model>> {
+    const accessToken = await this.facebookAuthentication.perform({ token })
 
     if (accessToken instanceof AccessToken) return ok({ accessToken: accessToken.value })
 
     return unauthorized()
   }
 
-  override buildValidators (httpRequest: HttpRequest): Validator[] {
+  override buildValidators ({ token }: HttpRequest): Validator[] {
     return [
-      ...Builder.of({ value: httpRequest.token, fieldName: 'token' }).required().build()
+      ...Builder.of({ value: token, fieldName: 'token' }).required().build()
     ]
   }
 }
