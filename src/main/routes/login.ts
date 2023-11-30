571b6272f5e543e4b6a485322b320098fa6970ff
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-11-30 17:30:03 -0300

    feat: changing callback

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,5 +1,7 @@
 import { type Router } from 'express'
+import { makeFacebookLoginController } from '@/main/factories/controllers'
+import { adaptExpressRoute as adapt } from '@/infra/http'
 
 export default (router: Router): void => {
-  router.post('/api/login/facebook', (req, res) => { res.send({ data: 'any_data' }) })
+  router.post('/api/login/facebook', adapt(makeFacebookLoginController()))
 }
