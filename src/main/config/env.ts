af62733e6a359f827ec540fa5b9683c7ef5d0898
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-12-01 17:58:21 -0300

    feat: creating dbConfig in env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,13 @@ export const env = {
     clientSecret: String(process.env.FB_CLIENT_SECRET) ?? '123456'
   },
   port: Number(process.env.PORT) ?? 8080,
-  jwtSecret: String(process.env.JWT_SECRET) ?? 'dad656'
+  jwtSecret: String(process.env.JWT_SECRET) ?? 'dad656',
+  dbConfig: {
+    type: String(process.env.DB_TYPE) ?? 'postgres',
+    host: String(process.env.DB_HOST) ?? 'motty.db.elephantsql.com',
+    port: Number(process.env.DB_PORT) ?? 5432,
+    username: String(process.env.DB_USERNAME) ?? 'test',
+    password: String(process.env.DB_PASSWORD) ?? 'test1234',
+    database: String(process.env.DB_NAME_DATABASE) ?? 'testDb'
+  }
 }
