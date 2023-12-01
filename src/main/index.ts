f199be70635b492d483d45a3c10bbb5aece67a11
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-12-01 18:10:44 -0300

    feat: creating connection with database

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,5 +3,9 @@ import 'reflect-metadata'
 import 'dotenv/config'
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
+import { createConnection } from 'typeorm'
+import { config } from '@/infra/postgres/helpers'
 
-app.listen(env.port, () => { console.log(`Server is running at http://localhost:${env.port}`) })
+createConnection(config)
+  .then(() => app.listen(env.port, () => { console.log(`Server is running at http://localhost:${env.port}`) }))
+  .catch(console.error)
