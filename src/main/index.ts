cf4565562b1c22c09661e57bba7221d59f97a60f
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-12-04 19:36:40 -0300

    fix: removing import config

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,8 +4,7 @@ import 'dotenv/config'
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 import { createConnection } from 'typeorm'
-import { config } from '@/infra/postgres/helpers'
 
-createConnection(config)
+createConnection()
   .then(() => app.listen(env.port, () => { console.log(`Server is running at http://localhost:${env.port}`) }))
   .catch(console.error)
