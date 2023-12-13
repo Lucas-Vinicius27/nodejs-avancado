d190d772301a4d49aeeaefb07bb8629277158d7a
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-12-12 22:24:25 -0300

    refactor: changing class name JwtTokenGenerator to JwtTokenHandler

--- src/main/factories/use-cases/facebook-authentication.ts
+++ src/main/factories/use-cases/facebook-authentication.ts
@@ -1,12 +1,12 @@
 import { setupFacebookAuthentication, type FacebookAuthentication } from '@/domain/use-cases'
 import { makePgUserAccountRepo } from '@/main/factories/repositories'
-import { makeJwtTokenGenerator } from '@/main/factories/crypto'
+import { makeJwtTokenHandler } from '@/main/factories/crypto'
 import { makeFacebookApi } from '@/main/factories/apis'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepo(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   )
 }
