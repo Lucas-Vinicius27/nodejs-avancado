02deae61bc523fcaed2bcd6cd8d5441fcd1ce605
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-12-15 08:51:45 -0300

    test: isolate creation sut

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -10,25 +10,25 @@ export class AuthenticationMiddleware {
 }
 
 describe('AuthenticationMiddleware', () => {
-  it('should return 403 if authorization is empty', async () => {
-    const sut = new AuthenticationMiddleware()
+  let sut: AuthenticationMiddleware
 
+  beforeEach(() => {
+    sut = new AuthenticationMiddleware()
+  })
+
+  it('should return 403 if authorization is empty', async () => {
     const httpResponse = await sut.handle({ authorization: '' })
 
     expect(httpResponse).toEqual({ statusCode: 403, data: new ForbiddenError() })
   })
 
   it('should return 403 if authorization is null', async () => {
-    const sut = new AuthenticationMiddleware()
-
     const httpResponse = await sut.handle({ authorization: null as any })
 
     expect(httpResponse).toEqual({ statusCode: 403, data: new ForbiddenError() })
   })
 
   it('should return 403 if authorization is undefined', async () => {
-    const sut = new AuthenticationMiddleware()
-
     const httpResponse = await sut.handle({ authorization: undefined as any })
 
     expect(httpResponse).toEqual({ statusCode: 403, data: new ForbiddenError() })
