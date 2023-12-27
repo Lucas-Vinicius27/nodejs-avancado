90452a41bdf3e2adbc4ae4a1d036e2d64e0a5ead
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-12-27 10:33:08 -0300

    feat: creating accessToken prop

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,8 @@
 export const env = {
   facebookApi: {
     clientId: String(process.env.FB_CLIENT_ID) ?? '123456',
-    clientSecret: String(process.env.FB_CLIENT_SECRET) ?? '123456'
+    clientSecret: String(process.env.FB_CLIENT_SECRET) ?? '123456',
+    accessToken: String(process.env.FB_ACCESS_TOKEN) ?? '123456'
   },
   port: Number(process.env.PORT) ?? 8080,
   jwtSecret: String(process.env.JWT_SECRET) ?? 'dad656'
