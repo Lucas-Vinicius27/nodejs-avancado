1810b078022f4c7e8425bcf7faab2e959e69cf5d
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2024-01-02 19:23:32 -0300

    refactor: changing import

--- src/main/factories/use-cases/facebook-authentication.ts
+++ src/main/factories/use-cases/facebook-authentication.ts
@@ -1,7 +1,6 @@
 import { setupFacebookAuthentication, type FacebookAuthentication } from '@/domain/use-cases'
 import { makePgUserAccountRepo } from '@/main/factories/repositories'
-import { makeJwtTokenHandler } from '@/main/factories/crypto'
-import { makeFacebookApi } from '@/main/factories/apis'
+import { makeFacebookApi, makeJwtTokenHandler } from '@/main/factories/gateways'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
