c556f264f64f7bcf34d5724155189011cde9ad1d
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2024-01-04 23:15:42 -0300

    feat: ensure ChangeProfilePicture calls SaveUserPicture with correct input when file is undefined

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -6,8 +6,9 @@ export type ChangeProfilePicture = (input: Input) => Promise<void>
 type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture) => ChangeProfilePicture
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfileRepo) => async ({ file, id }) => {
+  let pictureUrl: string | undefined
   if (file !== undefined) {
-    const pictureUrl = await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
-    await userProfileRepo.savePicture({ pictureUrl })
+    pictureUrl = await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
   }
+  await userProfileRepo.savePicture({ pictureUrl })
 }
